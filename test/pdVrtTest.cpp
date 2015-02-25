1ca6388ac7080978d17d7125d6f128c3c53fc2cc
Hiroshi Atsuta <atsuta@elblanco.com>
2015-02-25 17:36:17 +0900

    Rename 'vrt' ton 'vert'

--- test/pdVrtTest.cpp
+++ test/pdVrtTest.cpp
@@ -1,23 +1,23 @@
 #include "gtest/gtest.h"
-#include <pedi2/pd_vert.h>
+#include <pedi2/pd_vrt.h>
 
-TEST(pdVertTest, Init)
+TEST(pdVrtTest, Init)
 {
-  pdVert vert;
+  pdVrt vrt;
 
-  pdVertInit( &vert );
-  EXPECT_EQ( 0, vert.z );
+  pdVrtInit( &vrt );
+  EXPECT_EQ( 0, vrt.z );
 }
 
-TEST(pdVertTest, UpdateZeta)
+TEST(pdVrtTest, UpdateZeta)
 {
-  pdVert vert;
+  pdVrt vrt;
 
-  pdVertUpdateZeta( &vert, 0.26 );
-  EXPECT_EQ( 0.26, vert.z );
-  EXPECT_EQ( sqrt(RK_G/0.26), vert.zeta );
+  pdVrtUpdateZeta( &vrt, 0.26 );
+  EXPECT_EQ( 0.26, vrt.z );
+  EXPECT_EQ( sqrt(RK_G/0.26), vrt.zeta );
 
-  pdVertUpdateZeta( &vert, 0.3 );
-  EXPECT_EQ( 0.3, vert.z );
-  EXPECT_EQ( sqrt(RK_G/0.3), vert.zeta );
+  pdVrtUpdateZeta( &vrt, 0.3 );
+  EXPECT_EQ( 0.3, vrt.z );
+  EXPECT_EQ( sqrt(RK_G/0.3), vrt.zeta );
 }

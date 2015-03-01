5046fb632ac47da571a0bdeb6bc2a913f7938a11
Hiroshi Atsuta <atsuta@elblanco.com>
2015-03-02 06:09:56 +0900

    Fix ctrl_test for new pdCtrl

--- example/ctrl_test.c
+++ example/ctrl_test.c
@@ -1,5 +1,5 @@
 #include <zm/zm_ode.h>
-#include <pedi2/pedi2_ctrl.h>
+#include <pedi2/pd_ctrl.h>
 
 /* state vector
  * p = [ x  dx/dt  y  dy/dt ]^T
@@ -11,14 +11,14 @@ void polar(zVec p, double *r, double *theta)
   *theta = atan2( zVecElem(p,2), zVecElem(p,0) );
 }
 
-void measure(pedi2Ctrl *ctrl, zVec p, double *du, double *vu, double *dw, double *vw)
+void measure(pdCtrl *ctrl, zVec p, double *du, double *vu, double *dw, double *vw)
 {
   double r, rd;
   double theta, thetad;
   double s, c;
 
   polar( p, &r, &theta );
-  rd = 1.0 / ctrl->kappa;
+  rd = 1.0 / pdCtrlKappa(ctrl);
   thetad = theta;
   zSinCos( theta, &s, &c );
   *du = rd * ( thetad - theta );
@@ -42,16 +42,16 @@ zVec dp(double t, zVec p, void *dummy, zVec v)
 {
   double du, vu, dw, vw;
   double xz, yz;
-  pedi2Ctrl *ctrl;
+  pdCtrl *ctrl;
 
-  ctrl = (pedi2Ctrl *)dummy;
+  ctrl = (pdCtrl *)dummy;
   measure( ctrl, p, &du, &vu, &dw, &vw );
-  pedi2CtrlZMP( ctrl, du, vu, dw, vw );
-  world_zmp( p, ctrl->uz, ctrl->wz, &xz, &yz );
+  pdCtrlUpdate( ctrl, du, vu, dw, vw );
+  world_zmp( p, pdCtrlZMPTan(ctrl), pdCtrlZMPRad(ctrl), &xz, &yz );
   zVecElem(v,0) = zVecElem(p,1);
-  zVecElem(v,1) = zSqr(ctrl->vert->zeta) * ( zVecElem(p,0) - xz );
+  zVecElem(v,1) = zSqr(pdCtrlZeta(ctrl)) * ( zVecElem(p,0) - xz );
   zVecElem(v,2) = zVecElem(p,3);
-  zVecElem(v,3) = zSqr(ctrl->vert->zeta) * ( zVecElem(p,2) - yz );
+  zVecElem(v,3) = zSqr(pdCtrlZeta(ctrl)) * ( zVecElem(p,2) - yz );
   return v;
 }
 
@@ -61,32 +61,21 @@ int main(void)
 {
   zODE ode;
   zVec p;
-  pedi2Ctrl ctrl;
-  pedi2Vert vert;
+  pdCtrl ctrl;
   double t, xz, yz;
   register int i;
 
   zODEAssign( &ode, RKF45, NULL, NULL );
   zODEInit( &ode, 4, 0, dp );
-  pedi2CtrlInit( &ctrl );
-  ctrl.vert = &vert;
-  ctrl.vert->z = 0.26;
-  pedi2VertUpdate( ctrl.vert );
-  ctrl.qu1 = 1.0;
-  ctrl.qu2 = 0.0;
-  ctrl.qw1 = 1.0;
-  ctrl.qw2 = 1.5;
-  ctrl.kappa = 2.0;
-  ctrl.rho = 1.0;
-  ctrl.kr = 1.0;
-  /* ctrl.vd = 0.25; */
-  ctrl.vd = 0.5;
-  ctrl.w = 0.1;
+  pdCtrlInit( &ctrl );
+  pdCtrlSetPrm( &ctrl, 1.0, 0.0, 1.0, 1.5, 2.0, 1.0, 1.0 );
+  pdCtrlSetRefVrt( &ctrl, 0.26 );
+  pdCtrlSetRefHrz( &ctrl, 0.25, 0.0, 0.1 );
   p = zVecCreateList( 4, 0.6, 0.0, 0.0, 0.0 );
   for( i=0; i<=STEP; i++ ){
     t = DT * i;
     zODEUpdate( &ode, t, p, DT, &ctrl );
-    world_zmp( p, ctrl.uz, ctrl.wz, &xz, &yz );
+    world_zmp( p, pdCtrlZMPTan(&ctrl), pdCtrlZMPRad(&ctrl), &xz, &yz );
     printf( "%g %g %g ", t, xz, yz );
     zVecDataWrite( p );
     /* getchar(); */

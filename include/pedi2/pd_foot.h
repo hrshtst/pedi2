0e6506ad7d3c7023e95b09897a42f75a570cdb4e
Hiroshi Atsuta <atsuta@elblanco.com>
2015-03-25 02:23:58 +0900

    Add pdFoot class

---
+++ include/pedi2/pd_foot.h
@@ -0,0 +1,25 @@
+#ifndef __PD_FOOT_H__
+#define __PD_FOOT_H__
+
+#include <zeo/zeo_vec3d.h>
+#include <pedi2/pd_cz.h>
+
+/* foot property (sideway properties only) */
+typedef struct{
+  double h;           /* maximum lifting height */
+  zVec3D p;           /* current position */
+  zVec3D ps;          /* desired position for IK seeds */
+  zVec3D pd;          /* desired position */
+  zVec3D a;           /* current attitude */
+  zVec3D as;          /* desired attitude for IK seeds */
+  double stride_x, track_kx, track_cx, track_xold;
+  double stride_y, track_ky, track_cy, track_yold;
+  double track_kz, track_cz, track_zold;
+  double dy;          /* direction from inner side to outer side */
+  double yin, yout;   /* supporting region */
+  double xsfor, xsaft; /* stepping range */
+  double ysin, ysout; /* stepping range */
+  double phase;       /* lifting phase */
+} pdFoot;
+
+#endif /* __PD_FOOT_H__ */

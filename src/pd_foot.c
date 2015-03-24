3b8c5571a40506cbc05085e9d1e43d7eefb90303
Hiroshi Atsuta <atsuta@elblanco.com>
2015-03-25 04:49:39 +0900

    Implement pdFootPhase

--- src/pd_foot.c
+++ src/pd_foot.c
@@ -2,5 +2,19 @@
 
 void pdFootPhase(pdFoot *pf, pdFoot *kf, double xd, double yd, double theta, zComplex *pz)
 {
-  kf->phase = 0;
+  double dr, d, da, r2, r;
+  double s, c;
+  zComplex p, penter;
+
+  zSinCos( theta, &s, &c );
+  r2 = zComplexSqrAbs(pz);
+  r  = sqrt(r2);
+  dr = -(zVec3DElem(&pf->p,zX)-xd)*c-(zVec3DElem(&pf->p,zY)-yd)*s - 0.5*pf->dy*pf->sole_w;
+  da = acos( fabs(dr) / r );
+  if( ( d = r2 - zSqr(dr) ) > 0 ){
+    zComplexCreate( &penter, dr, -pf->dy*sqrt(d) );
+    zComplexCDiv( pz, &penter, &p );
+    kf->phase = zLimit( 0.5*zComplexArg(&p)/da, 0, 1 );
+  } else
+    kf->phase = 0;
 }
--- test/pdFootTest.cpp
+++ test/pdFootTest.cpp
@@ -8,9 +8,11 @@ protected:
   virtual void SetUp() {
     pdCZInit( &ctrl );
     // left foot
+    zVec3DCreate( &lf.p, -1, 0, 0 );
     lf.dy = 1;
     lf.sole_w = 0.07;
     // right foot
+    zVec3DCreate( &rf.p,  1, 0, 0 );
     rf.dy = -1;
     rf.sole_w = 0.07;
   };
@@ -24,21 +26,33 @@ protected:
 
 TEST_F(pdFootTest, CalcFootPhaseDoubleSupport)
 {
-  zComplexCreate(&pz, 0, 0 );
-  pdFootPhase( &lf, &rf, 0, 0, 0, &pz );
-  pdFootPhase( &rf, &lf, 0, 0, 0, &pz );
-  EXPECT_EQ( 0, lf.phase );
-  EXPECT_EQ( 0, rf.phase );
-
   zComplexCreate(&pz, 0, 1 );
   pdFootPhase( &lf, &rf, 0, 0, 0, &pz );
   pdFootPhase( &rf, &lf, 0, 0, 0, &pz );
   EXPECT_EQ( 0, lf.phase );
-  EXPECT_EQ( 0, rf.phase );
+  EXPECT_EQ( 1, rf.phase );
 
+  zComplexCreate(&pz, 0, -1 );
+  pdFootPhase( &lf, &rf, 0, 0, 0, &pz );
+  pdFootPhase( &rf, &lf, 0, 0, 0, &pz );
+  EXPECT_EQ( 1, lf.phase );
+  EXPECT_EQ( 0, rf.phase );
+}
+
+TEST_F(pdFootTest, CalcFootPhaseSingleSupport)
+{
   zComplexCreate(&pz, 1, 0 );
   pdFootPhase( &lf, &rf, 0, 0, 0, &pz );
   pdFootPhase( &rf, &lf, 0, 0, 0, &pz );
-  EXPECT_EQ( 0, lf.phase );
-  EXPECT_EQ( 0, rf.phase );
+  EXPECT_DOUBLE_EQ( 0, lf.phase );
+  EXPECT_DOUBLE_EQ( 0.5, rf.phase );
+
+  zComplexCreate(&pz, -1, 0 );
+  pdFootPhase( &lf, &rf, 0, 0, 0, &pz );
+  pdFootPhase( &rf, &lf, 0, 0, 0, &pz );
+  EXPECT_DOUBLE_EQ( 0.5, lf.phase );
+  EXPECT_DOUBLE_EQ( 0, rf.phase );
 }
+
+// TODO: add test for calculation of foot phase
+//       when (dx, dy) != (0, 0) and theta != 0

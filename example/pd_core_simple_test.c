06885e7f0f4697ec4eed721bb03272bec6e22a74
Hiroshi Atsuta <atsuta@elblanco.com>
2015-03-27 17:36:12 +0900

    Update comments in pd_core_simple_test.c

--- example/pd_core_simple_test.c
+++ example/pd_core_simple_test.c
@@ -13,8 +13,8 @@ int main(void)
   pdCommandDefaultInit( &com );
   pdCoreInit( &ctrl, &com );
 
-  /* load kinematics/dynamics model file (.zkc) */
-  /* and inverse kinematics configuration file (.conf) */
+  /* load kinematics/dynamics model file (robot.zkc) */
+  /* and inverse kinematics configuration file (robot_ik.conf) */
   pdCoreLoad( &ctrl, "model/hydra.zkc", "model/hydra_ik.conf" );
 
   /* prepare joint displacement vector */
@@ -33,11 +33,15 @@ int main(void)
     com.vud = 0.25;
     com.kappa = 0.0;
 
+    /* feedback the current state */
+    /* if not given, the controller will automatically update it */
+    /* pdCoreSetState( &ctrl, x, y ); */
+
     /* update */
     pdCoreUpdate( &ctrl, DT );
 
     /* output */
-    /* you can obtain desired joint displacement as a zVec instance */
+    /* obtain desired joint displacement as a zVec instance */
     pdCoreGetJointDis( &ctrl, dis );
     printf( "%f ", DT );zVecWrite( dis ); /* output to stdout */
   }

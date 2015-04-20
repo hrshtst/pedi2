7b32107bbf504c13d0fa5b0df7f823bc16ae5bb5
Hiroshi Atsuta <atsuta@elblanco.com>
2015-04-20 23:24:35 +0900

    Modify to use methods to get each parameter

--- test/pdCZVrtTest.cpp
+++ test/pdCZVrtTest.cpp
@@ -12,10 +12,10 @@ class pdCZVrtTest : public testing::Test {
   };
 
   void SetRandPrm() {
-    vrt._zd  = 1;
-    vrt._q1  = 2;
-    vrt._q2  = 3;
-    vrt.mass = 4;
+    pdCZVrtSetRef( &vrt, 1 );
+    pdCZVrtSetMass( &vrt, 2 );
+    pdCZVrtSetQ1( &vrt, 3 );
+    pdCZVrtSetQ2( &vrt, 4 );
     vrt._reffz = 5;
     vrt._refacc = 6;
     vrt.zeta = 7;
@@ -28,10 +28,10 @@ TEST_F(pdCZVrtTest, Init)
 {
   SetRandPrm();
   pdCZVrtInit( &vrt );
-  EXPECT_EQ( 0, vrt._zd );
-  EXPECT_EQ( 0, vrt._q1 );
-  EXPECT_EQ( 0, vrt._q2 );
-  EXPECT_EQ( 0, vrt.mass );
+  EXPECT_EQ( 0, pdCZVrtGetRef( &vrt ) );
+  EXPECT_EQ( 0, pdCZVrtGetMass( &vrt ) );
+  EXPECT_EQ( 0, pdCZVrtGetQ1( &vrt ) );
+  EXPECT_EQ( 0, pdCZVrtGetQ2( &vrt ) );
   EXPECT_EQ( 0, vrt._reffz );
   EXPECT_EQ( 0, vrt._refacc );
   EXPECT_EQ( 0, vrt.zeta );
@@ -41,10 +41,10 @@ TEST_F(pdCZVrtTest, Destroy)
 {
   SetRandPrm();
   pdCZVrtDestroy( &vrt );
-  EXPECT_EQ( 0, vrt._zd );
-  EXPECT_EQ( 0, vrt._q1 );
-  EXPECT_EQ( 0, vrt._q2 );
-  EXPECT_EQ( 0, vrt.mass );
+  EXPECT_EQ( 0, pdCZVrtGetRef( &vrt ) );
+  EXPECT_EQ( 0, pdCZVrtGetMass( &vrt ) );
+  EXPECT_EQ( 0, pdCZVrtGetQ1( &vrt ) );
+  EXPECT_EQ( 0, pdCZVrtGetQ2( &vrt ) );
   EXPECT_EQ( 0, vrt._reffz );
   EXPECT_EQ( 0, vrt._refacc );
   EXPECT_EQ( 0, vrt.zeta );
@@ -53,33 +53,33 @@ TEST_F(pdCZVrtTest, Destroy)
 TEST_F(pdCZVrtTest, SetRef)
 {
   pdCZVrtSetRef( &vrt, 1 );
-  EXPECT_EQ( 1, vrt._zd );
+  EXPECT_EQ( 1, pdCZVrtGetRef( &vrt ) );
   pdCZVrtSetRef( &vrt, 10 );
-  EXPECT_EQ( 10, vrt._zd );
+  EXPECT_EQ( 10, pdCZVrtGetRef( &vrt ) );
 }
 
 TEST_F(pdCZVrtTest, SetMass)
 {
   pdCZVrtSetMass( &vrt, 1 );
-  EXPECT_EQ( 1, vrt.mass );
+  EXPECT_EQ( 1, pdCZVrtGetMass( &vrt ) );
   pdCZVrtSetMass( &vrt, 10 );
-  EXPECT_EQ( 10, vrt.mass );
+  EXPECT_EQ( 10, pdCZVrtGetMass( &vrt ) );
 }
 
 TEST_F(pdCZVrtTest, SetQ1)
 {
   pdCZVrtSetQ1( &vrt, 1 );
-  EXPECT_EQ( 1, vrt._q1 );
+  EXPECT_EQ( 1, pdCZVrtGetQ1( &vrt ) );
   pdCZVrtSetQ1( &vrt, 10 );
-  EXPECT_EQ( 10, vrt._q1 );
+  EXPECT_EQ( 10, pdCZVrtGetQ1( &vrt ) );
 }
 
 TEST_F(pdCZVrtTest, SetQ2)
 {
   pdCZVrtSetQ2( &vrt, 1 );
-  EXPECT_EQ( 1, vrt._q2 );
+  EXPECT_EQ( 1, pdCZVrtGetQ2( &vrt ) );
   pdCZVrtSetQ2( &vrt, 10 );
-  EXPECT_EQ( 10, vrt._q2 );
+  EXPECT_EQ( 10, pdCZVrtGetQ2( &vrt ) );
 }
 
 TEST_F(pdCZVrtTest, SetPrm)

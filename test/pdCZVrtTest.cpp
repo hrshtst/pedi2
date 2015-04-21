a33b47f2cf7638c7b42db33ecb2bc0fb787b84da
Hiroshi Atsuta <atsuta@elblanco.com>
2015-04-21 11:50:14 +0900

    Add test for pdCZVrtUpdate

--- test/pdCZVrtTest.cpp
+++ test/pdCZVrtTest.cpp
@@ -99,6 +99,33 @@ TEST_F(pdCZVrtTest, ClacZMP)
 TEST_F(pdCZVrtTest, CalcZeta)
 {
   EXPECT_DOUBLE_EQ( 7.0023753112783096, pdCZVrtCalcZeta( &vrt, 0.2, 0, 0 ) );
+  EXPECT_DOUBLE_EQ( 9.9028541340363088, pdCZVrtCalcZeta( &vrt, 0.1, 0, 0 ) );
   EXPECT_DOUBLE_EQ( 7.037986928092435, pdCZVrtCalcZeta( &vrt, 0.2, 0.1, 0 ) );
+  EXPECT_DOUBLE_EQ( 9.9532165655128786, pdCZVrtCalcZeta( &vrt, 0.1, 0.1, 0 ) );
   EXPECT_DOUBLE_EQ( 9.9532165655128786, pdCZVrtCalcZeta( &vrt, 0.2, 0.1, 0.1 ) );
+}
+
+TEST_F(pdCZVrtTest, Update)
+{
+  pdCZVrtSetPrm( &vrt, 0.2, 1.0, 1.0, 5 );
+  pdCZVrtUpdate( &vrt, 0.2, 0, 0, 0 );
+  EXPECT_DOUBLE_EQ( 9.806652, pdCZVrtRF(&vrt) );
+  EXPECT_DOUBLE_EQ( 0, pdCZVrtAcc(&vrt) );
+  EXPECT_DOUBLE_EQ( 0, pdCZVrtZMP(&vrt) );
+  EXPECT_DOUBLE_EQ( 7.0023753112783096, pdCZVrtZeta(&vrt) );
+  pdCZVrtUpdate( &vrt, 0.1, 0, 0, 0 );
+  EXPECT_DOUBLE_EQ( 14.709978, pdCZVrtRF(&vrt) );
+  EXPECT_DOUBLE_EQ( 4.903326, pdCZVrtAcc(&vrt) );
+  EXPECT_DOUBLE_EQ( 0, pdCZVrtZMP(&vrt) );
+  EXPECT_DOUBLE_EQ( 9.9028541340363088, pdCZVrtZeta(&vrt) );
+  pdCZVrtUpdate( &vrt, 0.1, 0.1, 0, 0 );
+  EXPECT_DOUBLE_EQ( 13.309502937744337, pdCZVrtRF(&vrt) );
+  EXPECT_DOUBLE_EQ( 3.5028509377443386, pdCZVrtAcc(&vrt) );
+  EXPECT_DOUBLE_EQ( 0, pdCZVrtZMP(&vrt) );
+  EXPECT_DOUBLE_EQ( 9.9028541340363088, pdCZVrtZeta(&vrt) );
+  pdCZVrtUpdate( &vrt, 0.1, 0.1, 0.1, 0 );
+  EXPECT_DOUBLE_EQ( 13.309502937744337, pdCZVrtRF(&vrt) );
+  EXPECT_DOUBLE_EQ( 3.5028509377443386, pdCZVrtAcc(&vrt) );
+  EXPECT_DOUBLE_EQ( 0, pdCZVrtZMP(&vrt) );
+  EXPECT_DOUBLE_EQ( 9.9532165655128786, pdCZVrtZeta(&vrt) );
 }

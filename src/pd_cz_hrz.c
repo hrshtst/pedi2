c74a429b30fd4122a9fb00d442a3a7073505624b
Hiroshi Atsuta <atsuta@elblanco.com>
2015-04-26 18:45:52 +0900

    Fix arguments of update function

--- src/pd_cz_hrz.c
+++ src/pd_cz_hrz.c
@@ -111,12 +111,19 @@ void pdCZHrzUpdate(pdCZHrz *hrz, double x, double y, double theta, double vx, do
   pdCZHrzSetRefTheta( hrz, thetad );
   if( sr )
     pdCZHrzXformSRWtoM( hrz, sr );
-  pdCZHrzXformWtoM( hrz, pdCZHrzRefPosX(hrz), pdCZHrzRefPosY(hrz), &pdCZHrzRefU(hrz), &pdCZHrzRefW(hrz) );
-  pdCZHrzCalcDiffToRefPos( hrz, pdCZHrzRefU(hrz), pdCZHrzRefW(hrz), &pdCZHrzDeltaU(hrz), &pdCZHrzDeltaW(hrz) );
-  pdCZHrzRotWtoM( hrz, pdCZHrzVelX(hrz), pdCZHrzVelY(hrz), &pdCZHrzVelU(hrz), &pdCZHrzVelW(hrz) );
-  pdCZHrzUpdateM( hrz, pdCZHrzDeltaU(hrz), pdCZHrzDeltaW(hrz), pdCZHrzVelU(hrz), pdCZHrzVelW(hrz) );
-  pdCZHrzXformMtoW( hrz, pdCZHrzZMPU(hrz), pdCZHrzZMPW(hrz), &pdCZHrzZMPX(hrz), &pdCZHrzZMPY(hrz) );
-  pdCZHrzRotMtoW( hrz, pdCZHrzAccU(hrz), pdCZHrzAccW(hrz), &pdCZHrzAccX(hrz), &pdCZHrzAccY(hrz) );
+  pdCZHrzXformWtoM( hrz, pdCZHrzRefPosX(hrz), pdCZHrzRefPosY(hrz),
+                        &pdCZHrzRefU(hrz),   &pdCZHrzRefW(hrz) );
+  pdCZHrzCalcDiffToRefPos( hrz, pdCZHrzRefU(hrz),    pdCZHrzRefW(hrz),
+                               &pdCZHrzDeltaU(hrz), &pdCZHrzDeltaW(hrz) );
+  pdCZHrzRotWtoM( hrz, pdCZHrzVelX(hrz),  pdCZHrzVelY(hrz),
+                      &pdCZHrzVelU(hrz), &pdCZHrzVelW(hrz) );
+  pdCZHrzUpdateM( hrz, pdCZHrzDeltaU(hrz), pdCZHrzVelU(hrz),
+                       pdCZHrzDeltaW(hrz), pdCZHrzVelW(hrz) );
+  pdCZHrzXformMtoW( hrz, pdCZHrzZMPU(hrz),  pdCZHrzZMPW(hrz),
+                        &pdCZHrzZMPX(hrz), &pdCZHrzZMPY(hrz) );
+  pdCZHrzRotMtoW( hrz, pdCZHrzAccU(hrz),  pdCZHrzAccW(hrz),
+                      &pdCZHrzAccX(hrz), &pdCZHrzAccY(hrz) );
+}
 
 void pdCZHrzFWrite(FILE *fp, pdCZHrz *hrz)
 {

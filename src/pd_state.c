eb59177a34887fffd3dd0db707b231f7cc96b1fd
Hiroshi Atsuta <atsuta@elblanco.com>
2015-05-09 20:03:33 +0900

    Add pdState class

---
+++ src/pd_state.c
@@ -0,0 +1,24 @@
+#include <pedi2/pd_state.h>
+
+void pdStateInit(pdState *state)
+{
+  zVec3DClear( &state->com_pos );
+  zVec3DClear( &state->com_vel );
+  zVec3DClear( &state->lf_pos );
+  zVec3DClear( &state->rf_pos );
+  zVec3DClear( &state->lh_pos );
+  zVec3DClear( &state->rh_pos );
+  zMat3DClear( &state->base_att );
+  zMat3DClear( &state->lf_att );
+  zMat3DClear( &state->rf_att );
+  zMat3DClear( &state->lh_att );
+  zMat3DClear( &state->rh_att );
+  zVec3DClear( &state->zmp );
+  state->fz = 0;
+  state->sr = NULL;
+}
+
+void pdStateDestroy(pdState *state)
+{
+  pdStateInit( state );
+}

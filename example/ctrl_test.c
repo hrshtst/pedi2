2777e44b7b870216e42e4f01daa2303a36f376ca
Hiroshi Atsuta <atsuta@mourinho>
2015-09-11 21:22:14 +0900

    Fix examples for pdBiped class
    pdCore class is now obsolete, which is separated into pdBiped class
    and pdRobot class.

--- example/ctrl_test.c
+++ example/ctrl_test.c
@@ -1,13 +1,15 @@
 #include <pedi2/pd_cmd.h>
-#include <pedi2/pd_core.h>
+#include <pedi2/pd_state.h>
+#include <pedi2/pd_biped.h>
+#include <pedi2/pd_robot.h>
 
 /* #define DEBUG_MODE */
 #define OUTPUT_PLOT_DATA
 
-void init_mighty(pdCore *ctrl, pdCmd *cmd)
+void init_mighty(pdRobot *robot, pdCmd *cmd)
 {
   /* load mighty */
-  if( !pdCoreLoad( ctrl, "model/mighty.zkc" ) )
+  if( !pdRobotLoad( robot, "model/mighty.zkc" ) )
     exit( EXIT_FAILURE );
   /* set referential values */
   cmd->zd = 0.26;
@@ -20,13 +22,13 @@ void init_mighty(pdCore *ctrl, pdCmd *cmd)
   /*   rk_anim model/mighty.zkc motion.zvs -pan -- -90 -x 0.4 -y -- -6 -z 0.3 */
 }
 
-void init_hydra(pdCore *ctrl, pdCmd *cmd)
+void init_hydra(pdRobot *robot, pdCmd *cmd)
 {
   /* load hydra */
-  if( !pdCoreLoad( ctrl, "model/hydra.zkc" ) )
+  if( !pdRobotLoad( robot, "model/hydra.zkc" ) )
     exit( EXIT_FAILURE );
   /* set referential values */
-  cmd->zd = 0.85;
+  cmd->zd = 0.95;
   cmd->dist = 0.25;
   cmd->lfh = 0.1;
   cmd->rfh = 0.1;
@@ -41,30 +43,41 @@ void init_hydra(pdCore *ctrl, pdCmd *cmd)
 int main(int argc, char *argv[])
 {
   pdCmd cmd;
-  pdCore ctrl;
+  pdState state;
+  pdBiped biped;
+  pdRobot robot;
   zVec dis;
   FILE *fp;
   register int i;
 
   /* initialization */
   pdCmdDefaultInit( &cmd );
-  pdCoreInit( &ctrl, &cmd, DT );
+  pdStateInit( &state );
+  pdBipedInit( &biped, &cmd, &state, DT );
+  pdRobotInit( &robot );
 
   /* parse arguments */
   if( argc > 1 ){
     if( !strcmp( argv[1], "mighty" ) )
-      init_mighty( &ctrl, &cmd );
+      init_mighty( &robot, &cmd );
     else if( !strcmp( argv[1], "hydra" ) )
-      init_hydra( &ctrl, &cmd );
+      init_hydra( &robot, &cmd );
     else{
       ZRUNERROR( "invalid argument: %s", argv[1] );
       exit( EXIT_FAILURE );
     }
   } else
-    init_mighty( &ctrl, &cmd );
+    init_mighty( &robot, &cmd );
 
   /* allocate displacement vector */
-  dis = zVecAlloc( pdCoreJointSize(&ctrl) );
+  dis = zVecAlloc( pdRobotJointSize(&robot) );
+
+  /* initialize robot state */
+  pdRobotUpdateState( &robot, &state );
+  pdBipedDefaultPoseInit( &biped );
+  pdRobotSetBipedRefVec( &robot, &biped );
+  pdRobotSolveIK( &robot );
+  pdRobotUpdateState( &robot, &state );
 
   /* output data for plotting */
   fp = fopen( "data.log", "w" );
@@ -75,31 +88,39 @@ int main(int argc, char *argv[])
     cmd.kappa = 0.0;
 
     /* update */
-    pdCoreUpdate( &ctrl );
+    pdBipedUpdate( &biped );
+    pdRobotSetBipedRefVec( &robot, &biped );
+    pdRobotSolveIK( &robot );
 
 #ifdef DEBUG_MODE
-    pdCZVrtWrite( pdCZVrtPtr( pdCoreCZPtr(&ctrl) ) );
-    pdCZHrzUWWrite( pdCZHrzUWPtr( pdCZHrzPtr( pdCoreCZPtr(&ctrl) ) ) );
-    pdCZHrzWrite( pdCZHrzPtr( pdCoreCZPtr(&ctrl) ) );
-    pdCZWrite( pdCoreCZPtr(&ctrl) );
-    pdFootWrite( pdCoreLFPtr(&ctrl), pdCoreRFPtr(&ctrl) );
-    pdRobotWrite( pdCoreRobotPtr(&ctrl) );
+    pdCZVrtWrite( pdCZVrtPtr( pdBipedCZPtr(&biped) ) );
+    pdCZHrzUWWrite( pdCZHrzUWPtr( pdCZHrzPtr( pdBipedCZPtr(&biped) ) ) );
+    pdCZHrzWrite( pdCZHrzPtr( pdBipedCZPtr(&biped) ) );
+    pdCZWrite( pdBipedCZPtr(&biped) );
+    pdFootWrite( pdBipedLFPtr(&biped), pdBipedRFPtr(&biped) );
+    pdRobotWrite( pdBipedRobotPtr(&biped) );
     getchar();
 #endif
 
     /* output */
-    zVecCopy( pdCoreJointDis( &ctrl ), dis );
+    zVecCopy( pdRobotJointDis( &robot ), dis );
 #ifndef DEBUG_MODE
     printf( "%f ", DT );zVecWrite( dis );
 #endif
 #ifdef OUTPUT_PLOT_DATA
-    pdCoreDataFWrite( fp, &ctrl );
+    pdBipedDataFWrite( fp, &biped );
 #endif
+
+    /* update state */
+    pdBipedUpdateState( &biped );
+    pdRobotUpdateState( &robot, &state );
   }
 
   /* destroy */
   fclose( fp );
-  pdCoreDestroy( &ctrl );
+  pdRobotDestroy( &robot );
+  pdBipedDestroy( &biped );
+  pdStateDestroy( &state );
   pdCmdDestroy( &cmd );
   return 0;
 }

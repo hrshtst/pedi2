4cd1fa438b1155dcccb5f522ab6d690d9a1b54d4
Hiroshi Atsuta <atsuta@mourinho>
2015-09-30 23:15:46 +0900

    Fix typo

--- include/pedi2/pd_robot.h
+++ include/pedi2/pd_robot.h
@@ -74,7 +74,7 @@ __EXPORT void pdRobotDefaultBipedInit(pdRobot *robot, pdBiped *biped, pdState *s
 
 /* methods to solve FK */
 __EXPORT void pdRobotLinkSetJointDis(pdRobot *robot, int id, double *dis);
-__EXPORT void pdRobotSetJointDis(pdRobot *robot, zIndex idx, zVec dis);
+__EXPORT void pdRobotSetJointDis(pdRobot *robot, zIndex index, zVec dis);
 __EXPORT void pdRobotFK(pdRobot *robot, zVec dis);
 
 /* methods to solve IK */

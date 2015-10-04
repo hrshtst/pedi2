8017ade21c8463977b5fc142b9c5fab48b04e17b
Hiroshi Atsuta <atsuta@mourinho>
2015-10-04 19:07:34 +0900

    Add test of sidewalk

---
+++ example/sidewalk_test.c
@@ -0,0 +1,74 @@
+#include <pedi2/pd_cmd.h>
+#include <pedi2/pd_state.h>
+#include <pedi2/pd_biped.h>
+#include <pedi2/pd_robot.h>
+
+#define DT   0.01
+#define STEP 1000
+int main(void)
+{
+  pdCmd cmd;      /* user-defined command values */
+  pdState state;  /* robot state */
+  pdBiped biped;  /* bipdal locomotion controller */
+  pdRobot robot;  /* robot model instance */
+  zVec dis;       /* joint displacement vector */
+  register int i;
+
+  /* initialization */
+  pdCmdDefaultInit( &cmd );
+  pdStateInit( &state );
+  pdBipedInit( &biped, &cmd, DT );
+  pdRobotInit( &robot );
+
+  /* load kinematics/dynamics model file (robot.zkc) */
+  if( !pdRobotLoad( &robot, "model/hydra.zkc" ) )
+    exit( EXIT_FAILURE );
+
+  /* prepare joint displacement vector */
+  dis = zVecAlloc( pdRobotJointSize(&robot) );
+
+  /* set referential values */
+  cmd.zd = 0.95;           /* COM height */
+  cmd.dist = 0.25;         /* distance of both feet */
+  cmd.lfh = cmd.rfh = 0.1; /* (maximal) foot lifting height */
+  cmd.vud = 0.0;           /* referential velocity (longitudinal) */
+  cmd.kappa = 0.0;         /* referential curvature for rotation */
+
+  /* initialize robot state */
+  pdRobotDefaultBipedInit( &robot, &biped, &state );
+
+  /* main loop */
+  for( i=0; i<STEP; i++ ){
+    /* modify command values */
+    if( i<800 )
+      /* cmd.vwd = 0.08; */
+      cmd.vwd = 0.12;
+    else
+      cmd.vwd = 0;
+
+    /* update controller */
+    pdBipedUpdate( &biped, &state );
+    pdRobotSetBipedRefVec( &robot, &biped );
+    pdRobotSolveIK( &robot, 0 );
+
+    /* output */
+    /* obtain desired joint displacement as a zVec instance */
+    zVecCopy( pdRobotJointDis( &robot ), dis );
+    /* you can visualize the motion by executing the following command, e.g. */
+    /*   $ rk_anim model/hydra.zkc motion.zvs -x 12 -z 1 */
+
+    printf( "%f ", DT );zVecWrite( dis );
+    /* pdBipedDataWrite( &biped ); */
+
+    /* update state */
+    pdBipedUpdateState( &biped, &state );
+    pdRobotUpdateState( &robot, &state );
+  }
+
+  /* destroy */
+  pdRobotDestroy( &robot );
+  pdBipedDestroy( &biped );
+  pdStateDestroy( &state );
+  pdCmdDestroy( &cmd );
+  return 0;
+}

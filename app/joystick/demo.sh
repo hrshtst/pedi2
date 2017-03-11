749e0bea1447ed22790c47486e2491b97852d67d
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2017-03-11 14:34:15 +0900

    Update window size

--- app/joystick/demo.sh
+++ app/joystick/demo.sh
@@ -1,4 +1,4 @@
 git checkout demo
 # git checkout demo.20161013
 make
-./joystick_ctrl ../model/mighty.zkc -env ../model/env/random_room.zkc -width 700 -height 700
+./joystick_ctrl ../model/mighty.zkc -env ../model/env/random_room.zkc -width 1000 -height 1000

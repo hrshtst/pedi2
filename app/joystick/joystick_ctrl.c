560cea41a96687180ff6595c57eeb73145a0013c
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2017-05-26 18:45:08 +0900

    Fix limits of commanded values

--- app/joystick/joystick_ctrl.c
+++ app/joystick/joystick_ctrl.c
@@ -101,8 +101,8 @@ void* joystickCtrlCommand(void *args)
     aviator_action( &av, NULL );
     pthread_setcanceltype( PTHREAD_CANCEL_DEFERRED, &last_tmp );
     cmd.vud   = -0.15 * av.pitch / JOYSTICK_CTRL_VAL_LIM;
-    cmd.vwd   = -0.15 * av.roll  / JOYSTICK_CTRL_VAL_LIM;
-    cmd.kappa = -2.5 * av.yaw   / JOYSTICK_CTRL_VAL_LIM;
+    cmd.vwd   = -0.1  * av.roll  / JOYSTICK_CTRL_VAL_LIM;
+    cmd.kappa = -2.5  * av.yaw   / JOYSTICK_CTRL_VAL_LIM;
     pthread_setcanceltype( PTHREAD_CANCEL_ASYNCHRONOUS, &last_tmp );
   }
   return NULL;

c5656d6885401920cd8218351334d34ea1f54c62
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2017-08-05 16:07:24 +0900

    Fix to check if it is inside git work tree when executing demo.sh

--- app/joystick/demo.sh
+++ app/joystick/demo.sh
@@ -1,4 +1,6 @@
-git checkout demo
-# git checkout demo.20161013
+if git rev-parse --is-inside-work-tree &> /dev/null; then
+    git checkout demo
+    # git checkout demo.20161013
+fi
 make
 ./joystick_ctrl ../model/mighty.zkc -env ../model/env/random_room.zkc -width 1000 -height 1000

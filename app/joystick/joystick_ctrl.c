8a4e10c723fb13029cccb15c5140d106f9867b8c
Hiroshi Atsuta <atsuta@ieee.org>
2025-10-29 21:50:05 +0900

    fix: Fix app/joystick_ctrl to reflect updates

--- app/joystick/joystick_ctrl.c
+++ app/joystick/joystick_ctrl.c
@@ -1,12 +1,14 @@
-#include <roki/rokigl.h>
-#include <roki/rkgl_glx.h>
+#include <roki_gl/roki_gl.h>
+#include <roki_gl/roki_glx.h>
 #include <zx11/zximage_png.h>
 #include <liw/liw_time.h>
 #include <pedi2/pd_cmd.h>
 #include <pedi2/pd_state.h>
 #include <pedi2/pd_biped.h>
 #include <pedi2/pd_robot.h>
+#if 0
 #include <aviator.h>
+#endif
 #include <pthread.h>
 
 #define JOYSTICK_CTRL_TITLE "Joystick Operation"
@@ -24,7 +26,7 @@ enum{
 };
 zOption opt[] = {
   { "title", NULL, "<title name>", "title of sequence", (char *)"anim", false },
-  { "env", NULL, "<.z3d/.zkc file>", "environment model file", NULL, false },
+  { "env", NULL, "<.z3d/.ztk file>", "environment model file", NULL, false },
   { "width", NULL, "<width>", "set window width", (char *)"500", false },
   { "height", NULL, "<height>", "set window height", (char *)"500", false },
   { "dt", NULL, "<dt value>", "time step", (char *)"0.01", false },
@@ -44,8 +46,7 @@ zOption opt[] = {
 static rkChain chain;
 static rkglChain gl_chain;
 
-static rkChain chain_env;
-static rkglChain ge;
+static rkglChain g_env;
 static int env = 0;
 static rkglCamera cam;
 static rkglLight light;
@@ -73,6 +74,7 @@ static bool is_recording = false;
 static bool is_cmdlogging = false;
 
 #define DEVFILE "/dev/input/js0"
+#if 0
 static aviator_t av;
 
 void roll_move(aviator_t *av, int value, void *arg){ av->roll = value; }
@@ -105,11 +107,12 @@ void* joystickCtrlCommand(void *args)
   }
   return NULL;
 }
+#endif
 
 void joystickCtrlUsage(void)
 {
-  eprintf( "Usage: joystick_ctrl <.zkc file> [options]\n" );
-  eprintf( "<.zkc file>\tkinematic chain model file\n" );
+  eprintf( "Usage: joystick_ctrl <.ztk file> [options]\n" );
+  eprintf( "<.ztk file>\tkinematic chain model file\n" );
   eprintf( "[options]\n" );
   zOptionHelp( opt );
   exit( 0 );
@@ -135,7 +138,7 @@ void joystickCtrlLoad(char modelfile[])
     exit( 1 );
 
   if( !rkChainReadFile( &chain, modelfile ) ||
-      !rkglChainLoad( &gl_chain, &chain, NULL ) ){
+      !rkglChainLoad( &gl_chain, &chain, NULL, &light ) ){
     ZOPENERROR( modelfile );
     exit( 1 );
   }
@@ -144,7 +147,7 @@ void joystickCtrlLoad(char modelfile[])
 
   pdRobotUpdateState( &robot, &state );
   pdBipedDefaultPoseInit( &biped, &state );
-  pdRobotSetBipedRefVec( &robot, &biped );
+  pdRobotBipedSetRefVec( &robot, &biped );
   pdRobotSolveIK( &robot, 0 );
   pdRobotUpdateState( &robot, &state );
 
@@ -155,22 +158,27 @@ void joystickCtrlLoad(char modelfile[])
 
 int joystickCtrlLoadEnv(void)
 {
-  char *sfx;
-  int entry;
+  rkglChainAttr attr;
+  rkChain chain_env;
+  zMShape3D ms_env;
+  int entry = -1;
 
-  sfx = zGetSuffix( opt[OPT_ENVFILE].arg );
-  if( !( strcmp( sfx, RK_CHAIN_SUFFIX ) == 0 ?
-         rkChainReadFile( &chain_env, opt[OPT_ENVFILE].arg ) :
-         rkChainMShape3DReadFile( &chain_env, opt[OPT_ENVFILE].arg  ) ) ){
+  rkglChainAttrInit( &attr );
+  rkChainInit( &chain_env );
+  if( rkChainReadFile( &chain_env, opt[OPT_ENVFILE].arg ) ){
+    if( !rkglChainLoad( &g_env, &chain_env, &attr, &light ) ) exit( 1 );
+    entry = rkglBeginList();
+    rkglChainDraw( &g_env );
+    glEndList();
+    rkChainDestroy( &chain_env );
+  } else if( zMShape3DReadZTK( &ms_env, opt[OPT_ENVFILE].arg ) ){
+    entry = rkglEntryMShape( &ms_env, attr.disptype, &light );
+    zMShape3DDestroy( &ms_env );
+  } else{
     ZOPENERROR( opt[OPT_ENVFILE].arg );
-    joystickCtrlUsage();
     exit( 1 );
   }
-  if( !rkglChainLoad( &ge, &chain_env, NULL ) ) exit( 1 );
-
-  entry = rkglBeginList();
-  rkglChainDraw( &ge );
-  glEndList();
+  if( entry < 0 ) exit( 1 );
   return entry;
 }
 
@@ -178,8 +186,8 @@ int _joystickCtrlFindLinkID(char *name)
 {
   register int i;
 
-  for( i=0; i<(int)rkChainNum( &chain_env ); i++ ){
-    if( !strcmp( name, rkChainLinkName( &chain_env, i ) ) )
+  for( i=0; i<(int)rkChainLinkNum( g_env.chain ); i++ ){
+    if( !strcmp( name, rkChainLinkName( g_env.chain, i ) ) )
       return i;
   }
   return -1;
@@ -193,57 +201,65 @@ void joystickCtrlSetStartPos(void)
 
   if( ( id = _joystickCtrlFindLinkID( "start" ) ) < 0 )
     return;
-  zVec3DCopy( rkChainLinkWldPos( &chain_env, id ), &v );
-  zMat3DCopy( rkChainLinkWldAtt( &chain_env, id ), &m );
+  zVec3DCopy( rkChainLinkWldPos( g_env.chain, id ), &v );
+  zMat3DCopy( rkChainLinkWldAtt( g_env.chain, id ), &m );
 
   pdRobotGetJointDisAll( &robot, dis );
-  zVecSetElem( dis, 0, zVec3DElem( &v, 0 ) );
-  zVecSetElem( dis, 1, zVec3DElem( &v, 1 ) );
+  zVecSetElem( dis, 0, v.e[0] );
+  zVecSetElem( dis, 1, v.e[1] );
   zMat3DToAA( &m, &v );
-  zVecSetElem( dis, 5, zVec3DElem( &v, 2 ) );
+  zVecSetElem( dis, 5, v.e[2] );
 
-  pdRobotResetPose( &robot, &biped, &state, dis );
+  pdRobotBipedResetPose( &robot, &biped, &state, dis );
 }
 
 void joystickCtrlInit(void)
 {
   short width, height;
-  GLfloat white[] = { 1.0, 1.0, 1.0, 1.0 };
 
   width = atoi( opt[OPT_WIDTH].arg );
   height = atoi( opt[OPT_HEIGHT].arg );
   zxWindowCreate( &win, 0, 0, width+8, height+32 );
   zxWindowSetTitle( &win, JOYSTICK_CTRL_TITLE );
   zxWindowOpen( &win );
-  zxWindowSetBG( &win, (char *)"lightgray" );
-  zxWindowSetFG( &win, (char *)"black" );
+  zxWindowSetBGColorByName( &win, (char *)"lightgray" );
+  zxWindowSetColorByName( &win, (char *)"black" );
   zxWindowClear( &win );
-  zxSetFont( &win, "-misc-fixed-medium-r-normal-*-24-*-*-*-*-*-*-*" );
+  zxWindowSetFont( &win, "-misc-fixed-medium-r-normal-*-24-*-*-*-*-*-*-*" );
 
   glwin = rkglWindowCreateGLX( &win, 4, 4, width, height, NULL );
-  rkglKeyEnableGLX( glwin );
-  rkglMouseEnableGLX( glwin );
+  rkglWindowKeyEnableGLX( glwin );
+  rkglWindowMouseEnableGLX( glwin );
   rkglWindowOpenGLX( glwin );
 
-  rkglBGSet( &cam, 0.6, 0.6, 0.6 );
-  rkglCALookAt( &cam, 8, 0, 0.3, 0, 0, 0.3, 0, 0, 1 );
+  rkglCameraInit( &cam );
+  rkglCameraSetBackground( &cam, 0.6, 0.6, 0.6 );
+  rkglCameraLookAt( &cam, 8, 0, 0.3, 0, 0, 0.3, 0, 0, 1 );
+  rkglSetDefaultCamera( &cam );
 
   glEnable( GL_LIGHTING );
-  rkglLightCreate( &light, 0, 0.6, 0.6, 0.6, 0.8, 0.8, 0.8, 0, 0, 0, 0 );
-  rkglLightSetPos( &light, 10, 0, 4 );
+  rkglLightCreate( &light, 0.6, 0.6, 0.6, 0.8, 0.8, 0.8, 0, 0, 0 );
+  rkglLightMove( &light, 10, 0, 4 );
 
   if( opt[OPT_ENVFILE].flag )
     env = joystickCtrlLoadEnv();
-  else
-    env = rkglGauge( zX, 6.0, zY, 6.0, 1.0, 0.2, white );
+  else{
+    env = rkglBeginList();
+    rkglRGBByStr( "white" );
+    glLineWidth( 0.5 );
+    rkglGauge( zX, 6.0, zY, 6.0, 0.2 );
+    glEndList();
+  }
 
+#if 0
   joystickCtrlAviatorInit();
   pthread_create( &thread, NULL, joystickCtrlCommand, (void *)NULL );
+#endif
 }
 
 bool joystickCtrlCommandArgs(int argc, char *argv[])
 {
-  zStrList arglist;
+  zStrAddrList arglist;
 
   if( argc <= 1 ) joystickCtrlUsage();
   zOptionRead( opt, argv, &arglist );
@@ -256,7 +272,7 @@ bool joystickCtrlCommandArgs(int argc, char *argv[])
   joystickCtrlLoad( zListHead( &arglist )->data );
   if( opt[OPT_ENVFILE].flag )
     joystickCtrlSetStartPos();
-  zStrListDestroy( &arglist, false );
+  zStrAddrListDestroy( &arglist );
   return true;
 }
 
@@ -266,12 +282,12 @@ void joystickCtrlSetCamera(void)
   double s, c;
   double xd, yd, zd;
 
-  theta = zVec3DElem( &state.base_att, 0 );
+  theta = state.torso_att.c.x;
   zSinCos( theta, &s, &c );
   xd = cmd.xd;
   yd = cmd.yd;
   zd = cmd.zd;
-  rkglCALookAt( &cam, xd-15*zd*c, yd-15*zd*s, 2.5*zd, xd, yd, zd, 0, 0, 1 );
+  rkglCameraLookAt( &cam, xd-15*zd*c, yd-15*zd*s, 2.5*zd, xd, yd, zd, 0, 0, 1 );
 }
 
 void joystickCtrlReshape(void)
@@ -280,21 +296,15 @@ void joystickCtrlReshape(void)
   double wx, wy;
 
   zxGetGeometry( glwin, &reg );
-  rkglVPCreate( &cam, 0, 0, reg.width, reg.height );
+  rkglCameraSetViewport( &cam, 0, 0, reg.width, reg.height );
   wx = 0.1;
-  wy = wx / rkglVPAspect( &cam );
-  rkglFrustum( &cam, -wx, wx, -wy, wy, 1, 20 );
+  wy = wx / rkglCameraViewportAspectRatio( &cam );
+  rkglCameraSetFrustum( &cam, -wx, wx, -wy, wy, 1, 20 );
 }
 
 void joystickCtrlDraw(void)
 {
-  if( opt[OPT_ENVFILE].flag ){
-    glCallList( env );
-  } else {
-    glDisable( GL_LIGHTING );
-    glCallList( env );
-    glEnable( GL_LIGHTING );
-  }
+  if( env ) glCallList( env );
   if( path > 0 )
     glCallList( path );
   rkglChainDraw( &gl_chain );
@@ -302,12 +312,12 @@ void joystickCtrlDraw(void)
 
 void joystickCtrlDisplay(void)
 {
-  rkglActivateGLX( glwin );
+  rkglWindowActivateGLX( glwin );
   rkglClear();
-  rkglCALoad( &cam );
   rkglLightPut( &light );
+  rkglCameraPut( &cam );
   joystickCtrlDraw();
-  rkglSwapBuffersGLX( glwin );
+  rkglWindowSwapBuffersGLX( glwin );
   rkglFlushGLX();
 }
 
@@ -345,7 +355,7 @@ void joystickCtrlCapture(void)
 
   sprintf( imgfile, "capture%05d.png", cnt++ );
   zxImageAllocDefault( &img, zxWindowWidth(&win), zxWindowHeight(&win) );
-  zxImageFromPixmap( &img, zxCanvas(&win), img.width, img.height );
+  zxImageFromPixmap( &img, zxWindowCanvas(&win), img.width, img.height );
   zxImageWritePNGFile( &img, imgfile );
   zxImageDestroy( &img );
 }
@@ -412,10 +422,13 @@ int joystickCtrlEvent(void)
 {
   switch( zxDequeueEvent() ){
   case Expose:
-  case ConfigureNotify: joystickCtrlReshape();   break;
+  case ConfigureNotify: joystickCtrlReshape();
+    break;
   case KeyPress:
-    if( joystickCtrlKeyPress() < 0 ) return -1;  break;
-  case KeyRelease: zxModkeyOff( zxKeySymbol() ); break;
+    if( joystickCtrlKeyPress() < 0 ) return -1;
+    break;
+  case KeyRelease: zxModkeyOff( zxKeySymbol() );
+    break;
   default: ;
   }
   return 0;
@@ -424,7 +437,7 @@ int joystickCtrlEvent(void)
 void joystickCtrlUpdate(void)
 {
   pdBipedUpdate( &biped, &state );
-  pdRobotSetBipedRefVec( &robot, &biped );
+  pdRobotBipedSetRefVec( &robot, &biped );
   pdRobotSolveIK( &robot, 0 );
   zVecCopy( pdRobotJointDis( &robot ), dis );
   rkChainFK( &chain, dis );
@@ -571,7 +584,9 @@ void joystickCtrlPlay(void)
 
 void joystickCtrlExit(void)
 {
+#if 0
   aviator_close( &av );
+#endif
   zVecFree( dis );
   rkglChainUnload( &gl_chain );
   rkChainDestroy( &chain );
@@ -580,11 +595,13 @@ void joystickCtrlExit(void)
   pdStateDestroy( &state );
   pdCmdDestroy( &cmd );
   glDeleteLists( env, 1 );
+  rkglWindowCloseGLX( glwin );
   rkglWindowDestroyGLX( glwin );
-  rkglCloseGLX();
   zxWindowDestroy( &win );
+#if 0
   pthread_cancel( thread );
   pthread_join( thread, NULL );
+#endif
 }
 
 int main(int argc, char *argv[])

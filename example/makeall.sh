e7e03917d212ee91352207eb5e334f4deb0ec9a4
Hiroshi Atsuta <atsuta@ieee.org>
2025-10-30 21:39:41 +0900

    fix: Fix script to compile sample programs

--- example/makeall.sh
+++ example/makeall.sh
@@ -1,14 +1,5 @@
 #!/bin/sh
 
-for sampledir in *
-do
-    if [ -d ${sampledir} ]; then
-	cd ${sampledir} > /dev/null
-	if [ -e makefile ]; then
-	    make $1
-	fi
-	cd - > /dev/null
-    fi
-done
+make $1
 
 exit 0
